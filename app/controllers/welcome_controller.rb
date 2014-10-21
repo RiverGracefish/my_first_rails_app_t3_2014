cd577493833088b46574dd89e19467597072bed6
Grace Yu <grace.yu4444@gmail.com>
2014-10-20 22:27:32 -0600

    add time and introduction

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,11 @@ class WelcomeController < ApplicationController
   end
 
   def time
-    time = Time.new
+    @time = Time.new
+  end
+
+  def introduce
+    @name1 = params[:name1].capitalize
+    @name2 = params[:name2].capitalize
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'hello/:name' => 'welcome#hello'
 
-  get 'time/new'
+  get 'time/now' => 'welcome#time'
 
+  get 'introduce/:name1/and/:name2' => 'welcome#introduce'
 end
